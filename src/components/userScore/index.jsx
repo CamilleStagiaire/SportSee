1789b9948c0a2af661460524abbd5eebd0a7d4f8
CamilleStagiaire <100408607+CamilleStagiaire@users.noreply.github.com>
2023-09-29 14:01:02 +0200

    radialBarChart

--- src/components/userScore/index.jsx
+++ src/components/userScore/index.jsx
@@ -1,4 +1,4 @@
-import { Legend, ResponsiveContainer, PieChart, Pie, Cell } from 'recharts';
+import { Legend, ResponsiveContainer, RadialBarChart, RadialBar, Cell } from 'recharts';
 import SCSS_COLORS from '../../sass/utils/variables.scss';
 
 const COLORS = {
@@ -7,34 +7,36 @@ const COLORS = {
 };
 
 const CustomLegend = () => {
-  return <div className="pieChart-legend"> Score </div>;
+  return <div className="radialBarChart -legend"> Score </div>;
 };
 
 const UserScore = ({ score }) => {
   const data = [
     { name: 'Score', value: score * 100 },
-    { name: 'Rest', value: 100 - score * 100 },
+    { name: 'Rest', value: 100 }
   ];
 
   return (
-    <div className="pieChart graph">
+    <div className="radialBarChart  graph">
       <ResponsiveContainer width="100%" height="100%">
-        <PieChart>
-          <Pie dataKey="value" cx="50%" cy="50%" outerRadius={75} data={[{ value: 100 }]} fill={COLORS.CIRCLE} paddingAngle={0} />
-          <Pie data={data}  cx="50%" cy="50%" innerRadius={75} outerRadius={85} dataKey="value" startAngle={-270} endAngle={90} cornerRadius={10} >
-            <Cell fill={COLORS.SCORE} stroke="none" />
-            <Cell fill="transparent" stroke="none" />
-          </Pie>
+        <RadialBarChart innerRadius="70%" outerRadius="90%" data={data} startAngle={90} endAngle={90 + 360} >
+           <circle cx="50%" cy="50%" r="31%" fill={COLORS.CIRCLE} />
+          <RadialBar dataKey="value"  cornerRadius={10} clockWise={true} >
+            <Cell key="score" fill={COLORS.SCORE} stroke="none" />
+            <Cell key="rest" fill="transparent" stroke="none" /> 
+          </RadialBar>
+         
           <text x="50%" y="45%" dy={5} textAnchor="middle">
-            <tspan className="pieChart-percentage"> {`${Math.round(score * 100)}%`} </tspan>
-            <tspan x="50%" dy={20} className="pieChart-text">  de votre  </tspan>
-            <tspan x="50%" dy={20} className="pieChart-text">  objectif  </tspan>
+            <tspan className="radialBarChart -percentage"> {`${Math.round(score * 100)}%`} </tspan>
+            <tspan x="50%" dy={20} className="radialBarChart -text">  de votre  </tspan>
+            <tspan x="50%" dy={20} className="radialBarChart -text">  objectif  </tspan>
           </text>
           <Legend content={<CustomLegend />} verticalAlign="top" />
-        </PieChart>
+        </RadialBarChart>
       </ResponsiveContainer>
     </div>
   );
 };
 
 export default UserScore;
+
